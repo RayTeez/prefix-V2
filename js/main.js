4b22c4099406c84e78417361a8b5f6fea1912c75
RayTeez <35398014+RayTeez@users.noreply.github.com>
2020-11-29 04:20:17 +0200

    new stuff
    some new stuff were added here and some where fixed i think

--- js/main.js
+++ js/main.js
@@ -325,6 +325,24 @@ function quaries(){
   }
 }
 
+// ----->>skewing
 
+const section = document.querySelector(".skewable");
+let currentPixel = window.pageYOffset
 
+//looper keeps running and keeps track of where the new pixel is
+const looper = function () {
+  const newPixel = window.pageYOffset;
+  const diff = newPixel - currentPixel
+  const speed = diff * 0.35;
+  
+  section.style.transform = "skewY(" + speed + "deg)"
+  
+  
+  currentPixel = newPixel;
+  
+  requestAnimationFrame(looper)
+}
+
+looper();
 
