3a1b5b63afc9c258d8610de639565c38aac7b293
RayTeez <35398014+RayTeez@users.noreply.github.com>
2021-01-09 22:37:30 +0200

    put data from database
    first part is good( i think!!!!)

--- js/others.js
+++ js/others.js
@@ -5,37 +5,37 @@ const projects=[
         {
             projectName:'Ceres',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img1.jpg','img/otherprojects/img2.jpg','../img/otherprojects/img3.jpg']
         },
         {
             projectName:'Kiwi',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img2.jpg','img2','img3']
         },
         {
             projectName:'Bolobedu',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img3.jpg','img2','img3']
         },
         {
             projectName:'Slices',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img4.jpg','img2','img3']
         },
         {
             projectName:'Vision Africa',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img5.jpg','img2','img3']
         },
         {
             projectName:'pRefix-re',
             projectCatergory:'Branding',
-            images:['img1','img2','img3']
+            images:['img/otherprojects/img6.jpg','img2','img3']
         }
-    ],[ //logo Designs
+    ],[ //Logo Design
         {
             projectName:'Phomolong Market',
-            projectCatergory:'Logo',
+            projectCatergory:'Logo Design',
             images:['img1','img2','img3']
         },
         {
@@ -75,35 +75,90 @@ const projects=[
             images:['img1','img2','img3']
         }
     ],
-
-
-
     
 ];
 
 const projectOne = document.querySelector('.project-one');
 const projectTwo = document.querySelector('.project-two');
-const projecThree = document.querySelector('.project-one');
-const projectFour = document.querySelector('.project-one');
-const projectFive = document.querySelector('.project-one');
+const projecThree = document.querySelector('.project-three');
 
 let getProject = null;
 
-projectOne.addEventListener('click',)
 
-let newlink = function(){
-    var projNumber= 'test'  /*projectOne.getElementById('id').textContent*/;
-    var queryString = "?para1=" + projNumber;
-    return  'OtherProject.html'+ queryString;
+
+
+// projectOne.addEventListener('click', showProject(this));
+
+function showProject(e){
+    let newlink = function(){
+        var projNumber=  e.getAttribute('id'); //<<<--------what
+        var queryString = "?para1=" + projNumber;
+        window.location.href = 'OtherProject.html' + queryString;
+        return  'OtherProject.html'+ queryString;
+    }
+
+
+ 
+    let getproject = newlink().split("=");
+
+    console.log(getproject[1]);
+     
+    let putProject=projects[getproject[1]][0].projectName
+    
+    
+    console.log(putProject);
+    
 }
 
-//maybe have an if statment here
+if(window.location.pathname=='/OtherProject.html'){
+    let getproject =window.location.search.split("=");
+    let putProject=projects[getproject[1]][1].projectName;
+    console.log(getproject[1]);
+    console.log(putProject);
 
-let getproject = newlink().split("=");
+    const heading =document.querySelector('.t-heading');
+    heading.textContent=projects[getproject[1]][0].projectCatergory;
 
-console.log(getproject[1]);
+const pressedImg =document.querySelector('.t-project');  
 
 
-let showProject=projects[getproject[1]][1].projectName;
 
-console.log(showProject);
+
+
+let test=()=>{
+    console.log('im hereeee')
+
+    for ( i = 0; i < projects[getproject[1]].length; i++) {
+
+        let firstData = projects[getproject[1]][i]
+
+        pressedImg.innerHTML+= 
+        
+        `<li class="box${i+1}" onclick=showProjectCont()>
+        <div>
+        <button class="project_name">
+            <div class="button-wrapper">
+                    <span>${i+1}</span>
+                        <p class="project-name">${firstData.projectName}</p>
+            </div>
+        </button>
+        <img src=${firstData.images[0]} alt="image goes here">
+        </div>
+        </li>
+        
+        `  ;
+        
+
+
+
+    }
+
+}
+
+test()
+}
+
+
+function showProjectCont(){
+    console.log('Lets pretend you can see the images😅')
+}
